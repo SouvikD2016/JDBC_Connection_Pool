5da1a8514a10dd9ac16d89931a6ac5fb70ced0a7
Souvik Dey <souvikpostjob01@gmail.com>
2017-01-04 11:13:34 +0530

    Publishing to Git. Day 02. Change 05
    Signed-off-by: Souvik Dey <souvikpostjob01@gmail.com>

--- src/main/java/com/jci/JDBC_Hive_Connection_Two.java
+++ src/main/java/com/jci/JDBC_Hive_Connection_Two.java
@@ -165,6 +165,7 @@ public class JDBC_Hive_Connection_Two {
                             	System.out.println("Publishing to Git. Attepmt 01. Change 02");
                             	System.out.println("Publishing to Git. Attepmt 01. Change 03");
                             	System.out.println("Publishing to Git. Day 02. Change 04");
+                            	System.out.println("Publishing to Git. Day 02. Change 05");
                             	
                             } /*catch (ClassNotFoundException e) {
 								// TODO: handle exception
